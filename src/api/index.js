5721f51bee2f8108012f9b4637fc4357db237347
Jakob <69308518+jseinarsson@users.noreply.github.com>
2022-01-30 21:42:01 +0000

    Delete and Add working
    have to add refresh after delete

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:3007/streetarts';
 export const fetchPosts = () => axios.get(`${url}/get`);
 export const createPost = (newPost) => axios.post(`${url}/add`, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/update/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/delete/${id}`)
--- src/components/Posts/Post/post.js
+++ src/components/Posts/Post/post.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
 import EditForm from './editForm';
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, currentId, setCurrentId }) => {
+    const dispatch = useDispatch();
+
     return (
         <div className="border-padding">
             <img className="maxwidth" src={post.image} alt={ `${post.title} by ${post.artist}` }/>
             <h4>{post.title}</h4>
             <button onClick={() => setCurrentId(post._id) }>Edit</button>
-            <button onClick={() => {} }>Delete</button>
+            <button onClick={() => dispatch(deletePost(post._id)) }>Delete</button>
 
             <EditForm currentId={currentId} setCurrentId={setCurrentId} />
         </div>

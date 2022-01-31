5637991bebf4c8399880a05f7985e7f2440eff92
Jakob <69308518+jseinarsson@users.noreply.github.com>
2022-01-31 20:39:50 +0000

    Updated routes to match new routes in backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3007/streetarts';
+const url = 'http://localhost:3007/posts/';
 
-export const fetchPosts = () => axios.get(`${url}/get`);
-export const createPost = (newPost) => axios.post(`${url}/add`, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/update/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/delete/${id}`)
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
